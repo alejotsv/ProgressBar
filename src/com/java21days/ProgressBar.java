4ee95a9357d74f9de0abc389f7681c5ee97a61ef
Alejandro Salas <asalas.wd@outlook.com>
2022-11-13 22:22:45 -0500

    Set up ProgressBar class and time wait

--- src/com/java21days/ProgressBar.java
+++ src/com/java21days/ProgressBar.java
@@ -1,7 +1,30 @@
 package com.java21days;
 
 import javax.swing.*;
+import java.awt.*;
+import java.time.LocalDateTime;
+import java.util.concurrent.TimeUnit;
 
 public class ProgressBar extends JFrame {
+    Dimension dim = new Dimension(200, 200);
+
+    ProgressBar(String title) throws InterruptedException {
+        super(title);
+        setSize(dim);
+        setLocationRelativeTo(null);
+        JPanel panel = new JPanel();
+
+        TimeUnit.SECONDS.sleep(1);
+
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+    String time(LocalDateTime dateTime){
+        String date = dateTime.toString();
+        String[] time = date.split("T");
+        System.out.println(time[1]);
+        return time[1];
+    }
 
 }
---
+++ src/com/java21days/Timer.java
@@ -0,0 +1,9 @@
+package com.java21days;
+
+public class Timer {
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("Starting class");
+    }
+
+
+}
